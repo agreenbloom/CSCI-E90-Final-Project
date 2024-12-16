e466172723d281164b864cc446360d347a514f48
Abby Greenbloom <abbygreenbloom@abbys-mbp-2.home>
2024-12-16 13:22:27 -0500

    successfully getting trivia questions

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../components/logIn.vue';
 import Trivia from '../components/triviaHome.vue';
-import SignUp from '../components/signUp.vue';  // Import the SignUp component
 
 const routes = [
+  
   {
     path: '/',
-    name: 'Login',
-    component: Login,
-  },
-  {
-    path: '/trivia',
     name: 'Trivia',
     component: Trivia,
-  },
-  {
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUp,  // Add the route for SignUp
-  },
-  {
-    path: '/forgotPassword',
-    name: 'ForgotPassword',
-    component: () => import('../components/forgotPassword.vue'),
-  },
-  {
-    path: '/startQuiz',
-    name: 'StartQuiz',
-    component: () => import('../components/startTriviaGame.vue'),
-  },
-  {
-    path: '/createTriviaQuestion',
-    name: 'CreateTriviaQuestion',
-    component: () => import('../components/createTriviaQuestion.vue'),
   },
 ];
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { getIdToken, getGuestCredentials, getCurrentUser, getCognitoUser} from './auth'; // Import the function to get the JWT token
-import { getGuestId, generateSessionId } from './utils';
 
-// Base URL of your API Gateway or backend
-const API_BASE_URL = 'https://7bgydjo949.execute-api.us-east-1.amazonaws.com/production';
+
+// Base URL of>> found in AWS API GATEWAY > Stage > Stage name > copy Invoke URL
+const API_BASE_URL = 'ADD YOUR API';
 
 // Create an Axios instance
 const apiClient = axios.create({
@@ -13,46 +12,13 @@ const apiClient = axios.create({
   },
 });
 
- async function getGuestToken() {
-  const guestCredentials = await getGuestCredentials();
-  return guestCredentials.IdentityId;  // Use the IdentityId as the token for guest users
-}
 
-export function confirmUserSignUp(username, confirmationCode) {
-  const user = getCognitoUser(username);
-
-  return new Promise((resolve, reject) => {
-    user.confirmRegistration(confirmationCode, true, (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-}
-
-// Add the JWT token to the request headers
-apiClient.interceptors.request.use(
-  async (config) => {
-    try {
-      const token = await getIdToken(); // Get the token asynchronously
-      config.headers['Authorization'] = `Bearer ${token}`; // Add the token to the Authorization header
-    } catch (error) {
-      const guestToken = await getGuestToken();
-      config.headers['Authorization'] = `Bearer ${guestToken}`; // Use the guest token if idToken fails
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 // Function to make a GET request
 export async function getTriviaQuestions(categoryId) {
+  // update get request with the URL created in API GATEWSAY
     try {
-      const response = await apiClient.get(`questions?category_id=${categoryId}`);
+      const response = await apiClient.get(`UPDATEHERE?category_id=${categoryId}`);
       const results = JSON.parse(response.data.body); // Assuming the response body is JSON formatted like { body: '{"results": [...]}' }
       return results.results;
     } catch (error) {
@@ -60,62 +26,4 @@ export async function getTriviaQuestions(categoryId) {
       throw error;
     }
   }
-// Function to make a POST request
-export async function submitTriviaAnswer(answerData) {
-  try {
-    const response = await apiClient.post('/scores', answerData);
-    return response.data;
-  } catch (error) {
-    console.error('Error submitting trivia answer', error);
-    throw error;
-  }
-}
 
-export async function createQuizSession() {
-  console.log('HERE CREATE QUIZ SESSION');
-  
-  const currentUser = getCurrentUser();
-
-  // Ensure we have valid pool data and user
-  if (!currentUser) {
-    console.error('Missing pool data or current user');
-    throw new Error('Username and Pool information are required.');
-  }
-
-  const userName = currentUser.username;
-
-  try {
-
-    // Prepare request data
-    const data = {
-      sessionId: generateSessionId(),
-      userName: userName,
-    };
-
-    // Make the POST request
-    const response = await apiClient.post(`${API_BASE_URL}/startQuiz`, data);
-    console.log('Quiz session created successfully:', response.data);
-    return response.data;
-  } catch (error) {
-    console.error('Error creating quiz session:', error);
-    throw error;
-  }
-}
-
-  
-  // Function for quiz users to join a session
-  export async function joinQuizSession(sessionId) {
-    try {
-      const guestId = getGuestId(); // Retrieve guest ID for guest users
-      const response = await apiClient.post(`${API_BASE_URL}/join-session/${sessionId}`, { guestId });
-      return response.data; // { userId, sessionId }
-    } catch (error) {
-      console.error('Error joining quiz session:', error);
-      throw error;
-    }
-  }
-  export const createTriviaQuestion = async (payload) => {
-    const response = await apiClient.post(`${API_BASE_URL}/createTriviaQuestion`, payload);
-    return response.data;
-    
-};
